c9bed5dd62895a29a810f9eab7976669c27c668f
Vincent Garde <67907585+CoopVincent@users.noreply.github.com>
2022-10-14 16:06:53 +0200

    debug: [master] Update tests to Tryton 6.4 (#9)
    Fix #00000

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.debug.tests.test_debug import suite
-except ImportError:
-    from .test_debug import suite
-
-__all__ = ['suite']
--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,8 +1,5 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import unittest
-
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
@@ -11,8 +8,4 @@ class DebugTestCase(ModuleTestCase):
     module = 'debug'
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            DebugTestCase))
-    return suite
+del ModuleTestCase
